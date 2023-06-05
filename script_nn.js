b06438fdec9011b47b0e916ead338560bed08bcc
APaludan <andreashp@outlook.com>
2023-06-05 13:09:07 +0200

    slet en masse ting

--- script_nn.js
+++ script_nn.js
@@ -1,5 +1,12 @@
-tf.setBackend('cpu');
-let model = null;
+const useNN = window.location.href.endsWith("_nn.html");
+let model;
+
+class Multiplier {
+    constructor(fromDirection, value) {
+        this.fromDirection = fromDirection;
+        this.value = value;
+    }
+}
 
 Date.prototype.getDKHours = function () {
     return parseInt(this.toLocaleTimeString("da-DK", { timeZone: "Europe/Copenhagen", hour: "2-digit", hour12: false }).split(".")[0]);
@@ -10,6 +17,8 @@ const wUrl = "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=57
 const sUrl = getSUrl();
 const tUrl = "https://api.met.no/weatherapi/oceanforecast/2.0/complete?lat=56.988425&lon=10.286659";
 const tableDiv = document.getElementById("tableDiv");
+const windDirMultiplierArray = buildWindDirMultiplierArray();
+
 
 try {
     update()
@@ -19,119 +28,119 @@ try {
 }
 
 async function update() {
-    Promise.all([
-        await tf.loadLayersModel('tf_model/model.json'),
-        fetch(wUrl).then(response => response.json()),
-        fetch(sUrl).then(response => response.json()),
-        fetch(tUrl).then(response => response.json())])
-        .then(([loadedModel, wData, sData, tData]) => {
-            // load model
-            model = loadedModel;
+    let wData, sData, tData;
+    if (useNN) {
+        [model, wData, sData, tData] = await Promise.all([
+            tf.loadLayersModel('tf_model/model.json'),
+            fetch(wUrl).then(response => response.json()),
+            fetch(sUrl).then(response => response.json()),
+            fetch(tUrl).then(response => response.json())]);
+        tf.setBackend('cpu');
+        tf.enableProdMode();
+    }
+    else {
+        [wData, sData, tData] = await Promise.all([
+            fetch(wUrl).then(response => response.json()),
+            fetch(sUrl).then(response => response.json()),
+            fetch(tUrl).then(response => response.json())]);
+    }
 
-            // set water temp
-            let span = document.createElement("span");
-            span.className = "waterTemp transition-no-transform";
-            span.textContent = Math.round(tData["properties"]["timeseries"][0]["data"]["instant"]["details"]["sea_water_temperature"]) + "°"
-            document.getElementById("waterTemp").appendChild(span);
+    // set water temp
+    let span = document.createElement("span");
+    span.className = "waterTemp transition-no-transform";
+    span.textContent = Math.round(tData.properties.timeseries[0].data.instant.details.sea_water_temperature) + "°"
+    document.getElementById("waterTemp").appendChild(span);
 
-            let index = 0;
+    let index = 0;
 
-            let timeseries = wData["properties"]["timeseries"];
-            while (new Date(timeseries[index]["time"]).getHours() != new Date().getHours()) {
-                index++;
-            }
+    let timeseries = wData.properties.timeseries;
+    while (new Date(timeseries[index]["time"]).getHours() != new Date().getHours()) {
+        index++;
+    }
 
-            let totalTime = 0;
+    let totalTime = 0;
 
-            let forecast = [];
-            let day = [];
-            for (let i = index; i < timeseries.length; i++) {
-                let hour = new Date(timeseries[i]["time"]);
-                if (hour.getDKHours() < 6) continue;
-                let details = timeseries[i]["data"]["instant"]["details"];
-                let temp = details["air_temperature"];
-                let wind = details["wind_speed"];
-                let direction = details["wind_from_direction"];
-                if (day[0] != undefined && hour.getDKHours() < day[day.length - 1].hour.getDKHours()) {
-                    forecast.push(day);
-                    day = [];
-                }
-                const t0 = performance.now();
-                day.push(new Hour(hour, temp, wind, direction));
-                const t1 = performance.now();
-                totalTime += t1 - t0;
-            }
+    let forecast = [];
+    let day = [];
+    for (let i = index; i < timeseries.length; i++) {
+        let hour = new Date(timeseries[i]["time"]);
+        if (hour.getDKHours() < 6) continue;
+        let details = timeseries[i].data.instant.details;
+        let temp = details.air_temperature;
+        let wind = details.wind_speed;
+        let direction = details.wind_from_direction;
+        if (day[0] != undefined && hour.getDKHours() < day[day.length - 1].hour.getDKHours()) {
+            forecast.push(day);
+            day = [];
+        }
+        const t0 = performance.now();
+        day.push(new ForecastHour(hour, temp, wind, direction));
+        const t1 = performance.now();
+        totalTime += t1 - t0;
+    }
 
-            console.log("Total inference time: " + totalTime + " milliseconds.");
+    console.log("Total " + (useNN ? "inference time " : "calc time ") + totalTime + " milliseconds.");
 
-            forecast.forEach((day, dIndex) => {
-                let div = document.createElement("div");
-                let date = document.createElement("h4");
-                date.style.marginBottom = "5px";
-                div.style.opacity = "0";
-                div.style.animationDelay = (dIndex * 0.2) + "s";
-                date.textContent = getDay(day[0].hour.getDay()) + " " + day[0].hour.toLocaleDateString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -5).replace(".", "/");
-                div.appendChild(date);
-                tableDiv.appendChild(div);
+    forecast.forEach((day, dIndex) => {
+        let div = document.createElement("div");
+        let date = document.createElement("h4");
+        date.style.marginBottom = "5px";
+        div.style.opacity = "0";
+        div.style.animationDelay = (dIndex * 0.2) + "s";
+        date.textContent = getDay(day[0].hour.getDay()) + " " + day[0].hour.toLocaleDateString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -5).replace(".", "/");
+        div.appendChild(date);
+        tableDiv.appendChild(div);
 
-                let table = document.createElement("table");
-                table.appendChild(getTableHeader());
-                let tbody = document.createElement("tbody");
+        let table = document.createElement("table");
+        table.appendChild(getTableHeader());
+        let tbody = document.createElement("tbody");
 
-                day.forEach(hour => {
-                    tbody.appendChild(hourRow(hour));
-                });
+        day.forEach(hour => {
+            tbody.appendChild(hourRow(hour));
+        });
 
-                table.appendChild(tbody);
-                div.appendChild(table);
+        table.appendChild(tbody);
+        div.appendChild(table);
 
-                let sunDiv = document.createElement("div");
-                sunDiv.style.display = "flex";
-                sunDiv.style.justifyContent = "space-evenly";
+        let sunDiv = document.createElement("div");
+        sunDiv.style.display = "flex";
+        sunDiv.style.justifyContent = "space-evenly";
 
-                let sunriseDiv = document.createElement("div");
-                sunriseDiv.style.display = "flex";
-                sunriseDiv.style.alignItems = "center";
-                sunriseDiv.style.marginLeft = "20px";
+        let sunriseDiv = document.createElement("div");
+        sunriseDiv.className = "sunrise-div"
 
-                {
-                    let img = document.createElement("img");
-                    img.style.height = "60px";
-                    img.style.margin = "-5px";
-                    img.src = "sunrise.svg";
-                    sunriseDiv.appendChild(img);
-                }
-                let sunrise = document.createElement("p");
-                sunrise.style.marginLeft = "10px";
-                sunrise.textContent = new Date(sData.location.time[dIndex].sunrise.time).toLocaleTimeString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -3).replace(".", ":");
-                sunriseDiv.appendChild(sunrise);
+        {
+            let img = document.createElement("img");
+            img.className = "sun-img"
+            img.src = "sunrise.svg";
+            sunriseDiv.appendChild(img);
+        }
+        let sunrise = document.createElement("p");
+        sunrise.style.marginLeft = "10px";
+        sunrise.textContent = new Date(sData.location.time[dIndex].sunrise.time).toLocaleTimeString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -3).replace(".", ":");
+        sunriseDiv.appendChild(sunrise);
 
-                let sunsetDiv = document.createElement("div");
-                sunsetDiv.style.display = "flex";
-                sunsetDiv.style.alignItems = "center";
-                sunsetDiv.style.marginRight = "20px";
-                {
-                    let img = document.createElement("img");
-                    img.style.height = "60px";
-                    img.style.margin = "-5px";
-                    img.src = "sunset.svg";
-                    sunsetDiv.appendChild(img);
-                }
-                let sunset = document.createElement("p");
-                sunset.style.marginLeft = "10px";
-                sunset.textContent = new Date(sData.location.time[dIndex].sunset.time).toLocaleTimeString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -3).replace(".", ":");
-                sunsetDiv.appendChild(sunset);
+        let sunsetDiv = document.createElement("div");
+        sunsetDiv.className = "sunset-div"
+        {
+            let img = document.createElement("img");
+            img.className = "sun-img"
+            img.src = "sunset.svg";
+            sunsetDiv.appendChild(img);
+        }
+        let sunset = document.createElement("p");
+        sunset.style.marginLeft = "10px";
+        sunset.textContent = new Date(sData.location.time[dIndex].sunset.time).toLocaleTimeString("da-DK", { timeZone: "Europe/Copenhagen" }).slice(0, -3).replace(".", ":");
+        sunsetDiv.appendChild(sunset);
 
-                sunDiv.appendChild(sunriseDiv);
-                sunDiv.appendChild(sunsetDiv);
-                div.appendChild(sunDiv);
-                div.className = "transition";
+        sunDiv.appendChild(sunriseDiv);
+        sunDiv.appendChild(sunsetDiv);
+        div.appendChild(sunDiv);
+        div.className = "transition";
 
-                tableDiv.appendChild(div);
-            });
-            document.getElementById("footer").className = "transition";
-        })
-        .catch(error => { console.log(error); alert(error) });
+        tableDiv.appendChild(div);
+    });
+    document.getElementById("footer").className = "transition";
 }
 
 
@@ -148,14 +157,14 @@ function hourRow(hour) {
     temp.textContent = hour.temp.toFixed(0) + "°";
     if (temp.textContent === "-0°") temp.textContent = "0°";
     wind.textContent = hour.wind.toFixed(1);
-    {
-        let img = document.createElement("img");
-        img.style.transform = "rotate(" + hour.toDirection + "deg)";
-        img.style.height = "20px";
-        img.style.marginLeft = "10px";
-        img.src = "arrow_lowres.png";
-        wind.appendChild(img);
-    }
+
+    let img = document.createElement("img");
+    img.style.transform = "rotate(" + hour.toDirection + "deg)";
+    img.style.height = "20px";
+    img.style.marginLeft = "10px";
+    img.src = "arrow_lowres.png";
+    wind.appendChild(img);
+
     score.textContent = hour.score;
 
     row.appendChild(time);
@@ -165,17 +174,71 @@ function hourRow(hour) {
     return row;
 }
 
-class Hour {
+class ForecastHour {
     constructor(hour, temp, wind, direction) {
         this.hour = hour
         this.temp = temp;
         this.wind = wind;
         this.fromDirection = direction;
         this.toDirection = (direction + 180) % 360;
-        const res = model.predict(tf.tensor([[hour.getDKHours(), temp, wind, direction]])).dataSync();
-        if (res < 0) res = 0;
-        this.score = Math.round(res);
+        this.score = calcButter(hour, temp, wind, direction);
     }
+}
+
+function calcButter(hour, temp, wind, direction) {
+    if (useNN) return calcButterNN(hour, temp, wind, direction);
+    let score = wind * windDirMultiplierArray[Math.round(direction)];
+    return Math.round(score);
+}
+
+function calcButterNN(hour, temp, wind, direction) {
+    const inputTensor = tf.tensor([[hour.getDKHours(), temp, wind, direction]])
+    const res = model.predict(inputTensor).dataSync();
+    if (res < 0) res = 0;
+    return Math.round(res);
+}
+
+// must have multiplier at 0 and 360
+function getMultipliers() {
+    return [
+        new Multiplier(0, 0.9), 
+        new Multiplier(80, 0.6),
+        new Multiplier(135, 0.35),
+        new Multiplier(180, 0.3),
+        new Multiplier(240, 0.5),
+        new Multiplier(300, 0.75),
+        new Multiplier(360, 0.9)
+    ];
+}
+
+function buildWindDirMultiplierArray() {
+    let array = [];
+    let multipliers = getMultipliers();
+
+    for (let i = 0; i < multipliers.length; i++) {
+        if (multipliers[i + 1] == undefined) break;
+        let start = multipliers[i].fromDirection;
+        let end = multipliers[i + 1].fromDirection;
+        for (let j = start; j <= end; j++) {
+            let distance = end - start;
+            array[j] = linearInterpolation(multipliers[i].value, multipliers[i + 1].value, (j - start) / distance);
+        }
+    }
+
+    let chart = document.getElementById("chart");
+    for (let i = 0; i < array.length; i += 1) {
+        let div = document.createElement("div");
+        div.style.height = array[i] * 100 + "px";
+        div.style.backgroundColor = scoreColor(Math.round(array[i] * 8));
+        div.style.flexGrow = "1";
+        chart.appendChild(div);
+    };
+    return array;
+}
+
+
+function linearInterpolation(a, b, t) {
+    return a + (b - a) * t;
 }
 
 function scoreColor(score) {
