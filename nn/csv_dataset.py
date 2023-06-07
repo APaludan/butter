347d51f1b5f06abd709ed54bdb07885e01a5ff3d
APaludan <andreashp@outlook.com>
2023-06-07 22:20:13 +0200

    xx

--- nn/csv_dataset.py
+++ nn/csv_dataset.py
@@ -4,9 +4,9 @@ import numpy as np
 def get_data(is_train: bool):
     dataset = None
     if is_train:
-        dataset = pd.read_csv("train.csv", names=["Hour", "Temperature", "WindSpeed", "WindDirection", "Score"])
+        dataset = pd.read_csv("nn/train.csv", names=["Hour", "Temperature", "WindSpeed", "WindDirection", "Score"])
     else:
-        dataset = pd.read_csv("val.csv", names=["Hour", "Temperature", "WindSpeed", "WindDirection", "Score"])
+        dataset = pd.read_csv("nn/val.csv", names=["Hour", "Temperature", "WindSpeed", "WindDirection", "Score"])
     dataset = dataset.copy()
     labels = dataset.pop("Score")
     return np.array(dataset), np.array(labels)
--- nn/main.py
+++ nn/main.py
@@ -4,33 +4,43 @@ from csv_dataset import get_data
 import tensorflowjs as tfjs
 
 training_data, training_labels = get_data(is_train=True)
-test_data, test_labels = get_data(is_train=False)
+validation_data, validation_labels = get_data(is_train=False)
 
-BATCH_SIZE  = 50
-EPOCHS  = 1000
+BATCH_SIZE = 50
+EPOCHS = 500
 LR = 1e-3
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64),
+    tf.keras.layers.Dense(64),
     tf.keras.layers.Dense(1)
 ])
 
-model.compile(loss = tf.keras.losses.MeanSquaredError(),
-                      optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=LR))
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
+              optimizer=tf.keras.optimizers.experimental.AdamW(learning_rate=LR))
 
 # model.build(input_shape=(None, 4))
 # model.load_weights("model.h5")
 
-model.fit(training_data, training_labels, epochs=EPOCHS, batch_size=BATCH_SIZE)
+model.fit(training_data,
+          training_labels,
+          epochs=EPOCHS,
+          batch_size=BATCH_SIZE,
+          shuffle=True,
+          validation_data=(validation_data, validation_labels),
+          validation_batch_size=BATCH_SIZE,
+          validation_freq=2)
 
+# ToDo: add test data
+# print("eval:")
+# model.evaluate(test_data, test_labels)
 
-model.save("model.h5")
+model.save("nn/model.h5")
 
-tfjs.converters.save_keras_model(model, "tfjs_model")
+tfjs.converters.save_keras_model(model, "nn/tfjs_model")
 
-
-run_data = np.array([[11, 16, 1.5, 135]]) # score 1
+#                     h,  t,  w,  dir
+run_data = np.array([[21, 10, 10, 0]])  # score 7
 
 res = model(run_data)
 print("res: ", res)
-print("Done!")
