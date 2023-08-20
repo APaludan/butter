1d536e4ca44f38d37addd1f1a123d44d092b4172
APaludan <andreashp@outlook.com>
2023-08-20 23:52:07 +0200

    dfg

--- data_script.js
+++ data_script.js
@@ -1,2 +1,20 @@
 fetch("https://ahpa.azurewebsites.net/api/butter/")
     .then(res => res.text().then(text => document.getElementById("data").innerHTML = text));
+
+const form = document.getElementById("form");
+const res = document.getElementById("res");
+const submitButton = document.getElementById("submitButton")
+if (form != null) {
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        submitButton.setAttribute("disabled", "");
+        password = document.getElementById("password").value;
+        score = document.getElementById("score").value;
+        fetch(`https://ahpa.azurewebsites.net/api/butter?&score=${score}&password=${password}`, { method: "POST" })
+            .then(response => res.innerHTML = "status = " + response.status)
+            .catch(error => { 
+                res.innerHTML = error;
+                submitButton.removeAttribute("disabled")
+            });
+    });
+}
--- extras.js
+++ extras.js
@@ -4,19 +4,3 @@ const debug = url.searchParams.get('debug') == 'true';
 if (debug) {
     document.getElementById("chartDiv").classList.toggle("hidden");
 }
-
-const form = document.getElementById("form");
-const res = document.getElementById("res");
-const submitButton = document.getElementById("submitButton")
-if (form != null) {
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        submitButton.setAttribute("disabled", "");
-        password = document.getElementById("password").value;
-        score = document.getElementById("score").value;
-        fetch(`https://ahpa.azurewebsites.net/api/butter?&score=${score}&password=${password}`, { method: "POST" })
-            .then(response => res.innerHTML = "status = " + response.status)
-            .catch(error => res.innerHTML = error)
-            .finally(() => submitButton.removeAttribute("disabled"));
-    });
-}
