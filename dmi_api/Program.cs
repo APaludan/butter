f11588f9a18ba0dce15b9e78b59eb2786943af3a
APaludan <andreashp@outlook.com>
2023-11-13 23:35:20 +0100

    ye

--- dmi_api/Program.cs
+++ dmi_api/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 using System.Text.Json;
@@ -21,8 +21,18 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-var url = $"http://0.0.0.0:{port}";
+var url = "";
+
+if (Environment.GetEnvironmentVariable("PORT") == null)
+{
+    url = "https://localhost:8080";
+}
+else
+{
+    var port = Environment.GetEnvironmentVariable("PORT");
+    url = $"http://0.0.0.0:{port}";
+}
+
 
 var app = builder.Build();
 
@@ -43,11 +53,11 @@ app.MapGet("/watertemp", async () =>
     var response = await new HttpClient().GetAsync($"https://dmigw.govcloud.dk/v1/forecastedr/collections/DKSS_LF/position?coords=POINT(9.94088%2057.04797)&crs=crs84&parameter-name=water-temperature-1m&datetime={dateString}%2B02:00&api-key=f2411e62-d181-43ed-af72-55ad422a6a9a");
     if (!response.IsSuccessStatusCode)
     {
-        return 505;
+        return Results.Problem("DMI virker ik :(");
     }
     var json = JsonSerializer.Deserialize<JsonElement>(response.Content.ReadAsStream());
     var temperature = json.GetProperty("ranges").GetProperty("water-temperature-1m").GetProperty("values").EnumerateArray().First().GetSingle();
-    return temperature;
+    return Results.Ok(temperature);
 })
 .WithName("GetWaterTemp")
 .WithOpenApi();
