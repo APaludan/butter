4311b0ec6f1be1fb4755adab37a5abefe90c7de0
andreas <andreashp@outlook.com>
2026-01-31 19:38:31 +0100

    Delete extras.js

--- js/extras.js
+++
@@ -1,7 +0,0 @@
-const url = new URL(window.location.href);
-const debug = url.searchParams.get('debug') == 'true';
-
-
-if (debug) {
-    document.getElementById("chartDiv").classList.toggle("hidden");
-}
